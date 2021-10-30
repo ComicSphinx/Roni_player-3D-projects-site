e3e0c531e289b9685628f06e8fae90c5fceba7f8
Daniil Maslov <invicems@gmail.com>
2021-10-30 21:53:03 +0400

    Теперь отправляется запрос на получение текстовых данных

--- frontend/resources/js/presentation.js
+++ frontend/resources/js/presentation.js
@@ -1,8 +1,21 @@
+const getTextData = async () => {
+    try { // в {id} надо будет динамически подставлять id презентации
+        return await axios.get('http://localhost:3000/api/getTextData/{id}')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const textData = getTextData()
+
+
 var app = new Vue({
     el: '.grid',
+    // потом сделать тут data: data (приравнять к переменной выше)
     data: {
         title: 'Title 1',
         presentationTitle: 'Presentation title',
-        description: 'Few words about work another string about<br> work and something else<br>third string about description. Description end.'
+        description: 'Few words about work another string about<br> work and something else<br>third string about description. Description end.',
+        seen: false
     }
 });
