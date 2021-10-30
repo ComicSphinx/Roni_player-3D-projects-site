7ea274cabda3a29ac71d3b468181edef6fa0c04b
Daniil Maslov <invicems@gmail.com>
2021-10-30 21:09:24 +0400

    Вынес значения полей в Js

---
+++ frontend/resources/js/presentation.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '.grid',
+    data: {
+        title: 'Title 1',
+        presentationTitle: 'Presentation title',
+        description: 'Few words about work another string about<br> work and something else<br>third string about description. Description end.'
+    }
+});
