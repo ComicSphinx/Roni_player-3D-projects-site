1636df4d2c5575d3b588174b70cad9ca08dbe484
Daniil Maslov <invicems@gmail.com>
2021-10-31 01:12:19 +0400

    mend

--- frontend/resources/js/presentation.js
+++ frontend/resources/js/presentation.js
@@ -1,23 +1,21 @@
 // @Author: Daniil Maslov (ComicSphinx)
 
-const getTextData = async () => {
-    try { // в {id} надо будет динамически подставлять id презентации
-        return await axios.get('http://127.0.0.1:5000/presentation')
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const textData = getTextData()
-
-
 var app = new Vue({
     el: '.grid',
-    // потом сделать тут data: data (приравнять к переменной выше)
-    data: {
-        title: 'Title 1',
-        presentationTitle: 'Presentation title',
-        description: 'Few words about work another string about<br> work and something else<br>third string about description. Description end.',
-        seen: false
+    data() {
+        return {
+            presentationTitle: null,
+            description: null,
+            title: null
+        };
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:5000/presentation')
+            .then(response => (
+                this.description = response.data.description,
+                this.presentationTitle = response.data.presentationTitle,
+                this.title = response.data.title
+                ))
     }
 });
