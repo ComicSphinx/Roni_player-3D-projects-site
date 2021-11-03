bda0d8a08bdd4a34472235d9d26c06990ba5fa25
ComicSphinx <maslov2daniil@gmail.com>
2021-11-04 00:57:31 +0400

    added image fields to presentation.js

--- frontend/resources/js/presentation.js
+++ frontend/resources/js/presentation.js
@@ -1,5 +1,7 @@
 // @Author: Daniil Maslov (ComicSphinx)
 
+const baseUrl = 'http://127.0.0.1:5000'
+
 const router = new VueRouter({
     routes: 
     [
@@ -19,16 +21,39 @@ var app = new Vue({
         return {
             presentationTitle: null,
             description: null,
-            title: null
+            title: null,
+            images: {
+                firstImage: '',
+                secondImage: '',
+                thirdImage: '',
+                fourthImage: '',
+                fifthImage: '',
+                sixthImage: '',
+                seventhImage: '',
+                eightImage: ''
+            }
         };
     },
+    // Загрузка данных
     mounted() {
         axios
-            .get('http://127.0.0.1:5000/presentation'+this.$route.path)
+            .get(baseUrl+'/presentationText'+this.$route.path)
             .then(response => (
                 this.description = response.data.description,
                 this.presentationTitle = response.data.presentationTitle,
                 this.title = response.data.title
-                ))
+                ));
+        axios
+            .get(baseUrl+'/presentationImages'+this.$route.path)
+            .then(response => (
+                this.firstImage = response.data,
+                this.secondImage = response.data,
+                this.thirdImage = response.data,
+                this.fourthImage = response.data,
+                this.fifthImage = response.data,
+                this.sixthImage = response.data,
+                this.seventhImage = response.data,
+                this.eightImage = response.data
+            ));
     }
 });
