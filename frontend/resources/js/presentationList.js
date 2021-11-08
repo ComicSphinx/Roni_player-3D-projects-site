9a6c538753f97d00fd02c8341291154ce76dba8b
ComicSphinx <maslov2daniil@gmail.com>
2021-11-08 09:19:34 +0400

    presentationsList mechanism is done

--- frontend/resources/js/presentationList.js
+++ frontend/resources/js/presentationList.js
@@ -1,6 +1,7 @@
-// @Author: Daniil Maslov
+// @Author: Daniil Maslov (ComicSphinx)
 
 const baseUrl = 'http://127.0.0.1:5000'
+const presentationPage = 'presentation.html#/'
 
 const router = new VueRouter({
     routes:
@@ -21,14 +22,6 @@ var app = new Vue({
                     page: null,
                     image: null
                 }
-                // {
-                //     page: "presentation.html#/1",
-                //     image: baseUrl+"/presentationImage/1"+"/mainImage"
-                // },
-                // {
-                //     page: "presentation.html#/2",
-                //     image: baseUrl+"/presentationImage/2"+"/mainImage"
-                // }
             ]
         }
     },
@@ -36,12 +29,9 @@ var app = new Vue({
         axios
             .get(baseUrl+'/presentationsList/')
             .then(response => (
-                // TODO: На первой итерации в page, image подается id - 2, на второй - undefined (хотя, по console.log в 42 стр видно, что идет 1, затем 2)
-
                 response.data.presentationIds.forEach(element => {
-                    console.log(element),
-                    page = response.data.presentationIds[element],
-                    image = baseUrl+"/presentationImage/"+response.data.presentationIds[element]+"/mainImage"
+                    page = presentationPage + response.data.presentationIds[element-1],
+                    image = baseUrl + "/presentationImage/" + response.data.presentationIds[element-1] + "/mainImage"
                     
                     this.presentations.push({
                         page, 
@@ -50,21 +40,4 @@ var app = new Vue({
                 })
             ))
     }
-    // mounted() {
-    //     axios
-    //         .get(baseUrl+'/presentationsList/')
-    //         .then(response => (
-    //             // хз, принимает ли он такой формат? 0: 1 ; 1: 2 или просто через запятую надо? 1, 2
-    //             // this.presentations.page += response.data.presentationId
-    //             // this.presentations.page += 1
-                
-    //             // попробую собирать изображение уже на месте
-    //             // this.presentation.image = baseUrl+"'/presentationImage/'"+this.presentation.id+"'/mainImage'"
-    //             // console.log(this.presentations.images[0])
-    //             // // this.presentations.ids.forEach(id => {
-    //             // //     this.presentations.images[id] = ("baseUrl+'/presentationImage/'"+this.presentations.ids[id]+"'/mainImage'")
-    //             // // }),
-    //             // // console.log(this.presentations.images)
-    //         ))
-    // }
 })
