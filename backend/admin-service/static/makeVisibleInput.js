346dcd0fcc11688c1ea7ef9cdb81edf301f1c637
ComicSphinx <maslov2daniil@gmail.com>
2021-12-29 21:44:47 +0400

    Создал функцию defineParentClass, определяющую родительский класс. Вынес туда дублирующийся код.

--- backend/admin-service/static/makeVisibleInput.js
+++ backend/admin-service/static/makeVisibleInput.js
@@ -1,41 +1,17 @@
-// TODO: Если в блоке несколько картинок, оно отображается для самой первой
-// TODO: В square блоке инпуты почему-то стоят в отрыве от картинок. Как итог - при переходе на инпут, он пропадает
+// @Author: Daniil Maslov (ComicSphinx)
 
 var images = document.querySelectorAll('img');
 images.forEach((item) => {
     item.addEventListener('mouseover', (event) => {
-        // определить название класса (строка), из которого вызван input
-        try {
-            var parentClass = item.closest('.pic').className;
-            // Получить этот класс
-            parentClass = document.getElementsByClassName(parentClass);
-        } catch (err) {
-            var parentClass = item.closest('.block').className;
-            // Получить этот класс
-            parentClass = document.getElementsByClassName(parentClass);
-        }
-
+        var parentClass = defineParentClass(item);
         var inputImage = parentClass[0].querySelector('input');
         inputImage.style.visibility = 'visible';
     });
 
     item.addEventListener('mouseout', (event) => {
-        try {
-            // определить название класса (строка), из которого вызван input
-            var parentClass = item.closest('.pic').className;
-            // Получить этот класс
-            parentClass = document.getElementsByClassName(parentClass);
-        } catch (err) {
-            // определить название класса (строка), из которого вызван input
-            var parentClass = item.closest('.block').className;
-            // Получить этот класс
-            parentClass = document.getElementsByClassName(parentClass);
-        }
-
+        var parentClass = defineParentClass(item);
         var inputImage = parentClass[0].querySelector('input');
-        // TODO: УБРАТЬ ЭТУ ХНЮ ИЛИ ДОРАБОТАТЬ, ЧТОБЫ VISIBILITY НЕ ПРОПАДАЛ ПРИ НАВЕДЕНИИ НА ИНПУТ
-        setTimeout(function() {inputImage.style.visibility = 'hidden'}, 300);
-        
+        inputImage.style.visibility = 'hidden';
     });
 })
 
@@ -49,3 +25,15 @@ inputs.forEach((item) => {
         item.style.visibility = 'hidden';
     });
 })
+
+function defineParentClass(item) {
+    var parentClass;
+    
+    try {
+        parentClass = item.closest('.pic').className;
+    } catch (err) {
+        parentClass = item.closest('.block').className;
+    }
+    parentClass = document.getElementsByClassName(parentClass);
+    return parentClass;
+}
