ee7648ff14030d15b6fb058363035ace76ec82f8
Daniil Maslov <maslov2daniil@gmail.com>
2022-02-06 15:58:29 +0400

    Вынес логику получения данных в PresentationService

--- app/presentation-service/PresentationService.py
+++ app/presentation-service/PresentationService.py
@@ -1,6 +1,6 @@
 # @Author: Daniil Maslov (ComicSphinx)
 
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, jsonify
 from flask.templating import render_template
 from flask_restful import Api, Resource
 import requests
@@ -8,30 +8,42 @@ from requests.exceptions import JSONDecodeError
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://gdjqbznobxhqxk:cf39ce62f344cfa9cb15eba8c31a6e0304f42750e5f69a3563da7505f1f80fb5@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d5hggk2n1g9v3n'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-DB_SERVICE_BASE_URL = 'http://127.0.0.1'
+# TODO: СКОРРЕКТИРОВАТЬ НАИМЕНОВАНИЕ И ПУТЬ
+UPLOAD_FOLDER_PATH = 'static/presentations/'
 
 class PresentationService(Resource):
 
     @app.route('/presentation/<id>', methods=['GET'])
     def presentation(id):
-        # Отправить запрос, получить ответ
-        url = DB_SERVICE_BASE_URL+'/presentation/'+id
-        data = requests.get(url)
-        try:
-            data = data.json()
-            return render_template('presentation.html', data=data)
-        except JSONDecodeError:
-            return redirect(url_for('presentationsList'))
+        # Получить презентацию
+        presentation = PresentationService.getPresentationById(id)
+        
+        # Возможно, надо будет делать .json
+        return render_template('presentation.html', data=presentation)
 
     @app.route('/presentationsList/', methods=['GET'])
     def presentationsList():
-        # Отправить запрос, получить ответ
-        url = DB_SERVICE_BASE_URL+'/getPresentationsListData'
-        data = requests.get(url).json()
+        # Получить список презентаций
+        presentationList = PresentationService.getPresentationsList()
 
-        return render_template('presentationsList.html', presentationsList=data)
+        # мб надо будет делать .json
+        return render_template('presentationsList.html', presentationsList=presentationList)
+
+    def getPresentationById(id):
+        presentation = Presentation.query.filter_by(id=id, active=True).first_or_404()
+        return Presentation.serialize(presentation)
+
+    def getPresentationsList():
+        presentationsList = Presentation.query.filter_by(active=True).all()
+        presentationsListSerialized = []
+
+        for i in presentationsList:
+            presentationsListSerialized.append(Presentation.serialize(i))
+
+        return jsonify(presentationsListSerialized)
 
 api.add_resource(PresentationService)
 if __name__ == "__main__":
